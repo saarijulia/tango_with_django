39503e0c9ba3950402120920f9f229b7c5693823
Julia Saari <mariajulia.saari@gmail.com>
2020-02-04 18:55:30 +0000

    Finished ch 4 - tests ok

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponse
 
 #creating an view called index - takes in an HttpResponse object
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a> ")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+    #return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a> ")
 # returning the HttpResponse object - sending this string to the content of the web
 
 # Create your views here.
 
 def about(request):
-   return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Julia'}
+    return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 #importing the view from the rango environment
 
@@ -26,4 +28,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
